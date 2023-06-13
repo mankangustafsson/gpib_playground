baea6443bbeeb09244dceed3970d2b81d532627d
mankangustafsson <mankan@lysator.liu.se>
2023-06-13 21:09:15 +0200

    Dump and restore scripts for ESG E44xx

--- e4407b.py
+++ e4407b.py
@@ -4,8 +4,8 @@ from datetime import datetime
 rm = pyvisa.ResourceManager()
 print(rm.list_resources())
 
-dev = rm.open_resource('USB0::0x03EB::0x2065::Hewlett-Packard__E4407B__MY41440643__A.14.06::INSTR')
-#dev = rm.open_resource('GPIB1::18::INSTR')
+#dev = rm.open_resource('USB0::0x03EB::0x2065::Hewlett-Packard__E4407B__MY41440643__A.14.06::INSTR')
+dev = rm.open_resource('GPIB1::18::INSTR')
 dev.timeout = 8000
 
 #dev.read_termination = '\n' 
@@ -15,7 +15,7 @@ s = dev.query('*IDN?')
 print('connected to ' + s)
 
 dev.write(":MMEM:DEL 'R:SCREEN.GIF';*CLS")
-for i in range(5):
+for i in range(1):
     dev.write(":MMEM:STOR:SCR 'R:SCREEN.GIF'")
     data = dev.query_binary_values(":MMEM:DATA? 'R:SCREEN.GIF'",
                                    is_big_endian=True, datatype='c')
---
+++ e44xx_restore_165.py
@@ -0,0 +1,27 @@
+import pyvisa
+
+rm = pyvisa.ResourceManager()
+print(rm.list_resources())
+dev = rm.open_resource('USB0::0x03EB::0x2065::Hewlett-Packard__ASG-A3000A__AA00000000__B.03.86::INSTR')
+dev.read_termination = '\n'
+dev.write_termination = '\n'
+dev.query(':SYST:PRES; *OPC?;')
+dev.write(':SERV:PRODUCTION:CAL:BEGIN;')
+dev.write(':SERV:PRODUCTION:CAL 165,0,3.93400631533203125E7;')
+dev.write(':SERV:PRODUCTION:CAL 165,1,50000.000000;')
+dev.write(':SERV:PRODUCTION:CAL 165,2,4000000000.000000;')
+dev.write(':SERV:PRODUCTION:CAL 165,5,7;')
+dev.write(':SERV:PRODUCTION:CAL 165,6,8;')
+dev.write(':SERV:PRODUCTION:CAL 165,7,5;')
+dev.write(':SERV:PRODUCTION:CAL 165,8,9;')
+dev.write(':SERV:PRODUCTION:CAL 165,10,4436.557800292969;')
+dev.write(':SERV:PRODUCTION:CAL 165,12,8000000;')
+dev.write(':SERV:PRODUCTION:CAL 165,13,8388608;')
+dev.write(':SERV:PRODUCTION:CAL 165,20,1;')
+dev.write(':SERV:PRODUCTION:CAL 165,21,4436.557800292969;')
+dev.write(':SERV:PRODUCTION:CAL 165,24,3000;')
+dev.write(':SERV:PRODUCTION:CAL:END;')
+dev.write(':SERV:PRODUCTION:CAL:STORE 165;')
+dev.write(':SERV:PRODUCTION:CAL:PUP;')
+print('done, now restart your unit')
+dev.close()
