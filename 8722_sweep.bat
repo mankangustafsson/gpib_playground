368fe33c9605ef6f995d9b752522e628389981cc
mankangustafsson <mankan@lysator.liu.se>
2023-11-12 20:30:34 +0100

    Working HP8722 sweep with Marconi 6960

---
+++ 8722_sweep.bat
@@ -0,0 +1,161 @@
+@echo OFF
+.\vna_cw.py -f 1G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 1G read
+
+.\vna_cw.py -f 2G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 2G read
+
+.\vna_cw.py -f 3G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 3G read
+
+.\vna_cw.py -f 4G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 4G read
+
+.\vna_cw.py -f 5G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 5G read
+
+.\vna_cw.py -f 6G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 6G read
+
+.\vna_cw.py -f 7G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 7G read
+
+.\vna_cw.py -f 8G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 8G read
+
+.\vna_cw.py -f 9G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 9G read
+
+.\vna_cw.py -f 10G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 10G read
+
+.\vna_cw.py -f 11G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 11G read
+
+.\vna_cw.py -f 12G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 12G read
+
+.\vna_cw.py -f 13G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 13G read
+
+.\vna_cw.py -f 14G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 14G read
+
+.\vna_cw.py -f 15G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 15G read
+
+.\vna_cw.py -f 16G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 16G read
+
+.\vna_cw.py -f 17G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 17G read
+
+.\vna_cw.py -f 18G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 18G read
+
+.\vna_cw.py -f 19G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 19G read
+
+.\vna_cw.py -f 20G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 20G read
+
+.\vna_cw.py -f 21G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 21G read
+
+.\vna_cw.py -f 22G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 22G read
+
+.\vna_cw.py -f 23G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 23G read
+
+.\vna_cw.py -f 24G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 24G read
+
+.\vna_cw.py -f 25G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 25G read
+
+.\vna_cw.py -f 26G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 26G read
+
+.\vna_cw.py -f 27G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 27G read
+
+.\vna_cw.py -f 28G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 28G read
+
+.\vna_cw.py -f 29G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 29G read
+
+.\vna_cw.py -f 30G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 30G read
+
+.\vna_cw.py -f 31G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 31G read
+
+.\vna_cw.py -f 32G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 32G read
+
+.\vna_cw.py -f 33G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 33G read
+
+.\vna_cw.py -f 34G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 34G read
+
+.\vna_cw.py -f 35G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 35G read
+
+.\vna_cw.py -f 36G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 36G read
+
+.\vna_cw.py -f 37G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 37G read
+
+.\vna_cw.py -f 38G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 38G read
+
+.\vna_cw.py -f 39G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 39G read
+
+.\vna_cw.py -f 39.5G -d -20.0 -p 2
+timeout 1 >nul
+.\m6960.py -f 39.5G read
+
---
+++ gen_8722_sweep.py
@@ -0,0 +1,7 @@
+print('@echo OFF')
+for n in range(40):
+    print(f'.\\vna.py -f {n + 1}G -d -10.0 -p 1')
+    print('timeut 1 >nul')
+    print(f'.\\m6960.py -f {n + 1}G read')
+    print()
+print('remember to change character encoding to non-UTF')
