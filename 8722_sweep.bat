4339bb28173e15fd208cf3dd220678aba82a99bc
mankangustafsson <mankan@lysator.liu.se>
2025-04-14 21:04:25 +0200

    Prepare for new probe testing

--- 8722_sweep.bat
+++ 8722_sweep.bat
@@ -1,161 +1,161 @@
 @echo OFF
 .\vna_cw.py -f 1G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 1G read
+.\438a.py -a 4 -f 1G read
 
 .\vna_cw.py -f 2G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 2G read
+.\438a.py -a 4 -f 2G read
 
 .\vna_cw.py -f 3G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 3G read
+.\438a.py -a 4 -f 3G read
 
 .\vna_cw.py -f 4G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 4G read
+.\438a.py -a 4 -f 4G read
 
 .\vna_cw.py -f 5G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 5G read
+.\438a.py -a 4 -f 5G read
 
 .\vna_cw.py -f 6G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 6G read
+.\438a.py -a 4 -f 6G read
 
 .\vna_cw.py -f 7G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 7G read
+.\438a.py -a 4 -f 7G read
 
 .\vna_cw.py -f 8G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 8G read
+.\438a.py -a 4 -f 8G read
 
 .\vna_cw.py -f 9G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 9G read
+.\438a.py -a 4 -f 9G read
 
 .\vna_cw.py -f 10G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 10G read
+.\438a.py -a 4 -f 10G read
 
 .\vna_cw.py -f 11G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 11G read
+.\438a.py -a 4 -f 11G read
 
 .\vna_cw.py -f 12G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 12G read
+.\438a.py -a 4 -f 12G read
 
 .\vna_cw.py -f 13G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 13G read
+.\438a.py -a 4 -f 13G read
 
 .\vna_cw.py -f 14G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 14G read
+.\438a.py -a 4 -f 14G read
 
 .\vna_cw.py -f 15G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 15G read
+.\438a.py -a 4 -f 15G read
 
 .\vna_cw.py -f 16G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 16G read
+.\438a.py -a 4 -f 16G read
 
 .\vna_cw.py -f 17G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 17G read
+.\438a.py -a 4 -f 17G read
 
 .\vna_cw.py -f 18G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 18G read
+.\438a.py -a 4 -f 18G read
 
 .\vna_cw.py -f 19G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 19G read
+.\438a.py -a 4 -f 19G read
 
 .\vna_cw.py -f 20G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 20G read
+.\438a.py -a 4 -f 20G read
 
 .\vna_cw.py -f 21G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 21G read
+.\438a.py -a 4 -f 21G read
 
 .\vna_cw.py -f 22G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 22G read
+.\438a.py -a 4 -f 22G read
 
 .\vna_cw.py -f 23G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 23G read
+.\438a.py -a 4 -f 23G read
 
 .\vna_cw.py -f 24G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 24G read
+.\438a.py -a 4 -f 24G read
 
 .\vna_cw.py -f 25G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 25G read
+.\438a.py -a 4 -f 25G read
 
 .\vna_cw.py -f 26G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 26G read
+.\438a.py -a 4 -f 26G read
 
 .\vna_cw.py -f 27G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 27G read
+.\438a.py -a 4 -f 27G read
 
 .\vna_cw.py -f 28G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 28G read
+.\438a.py -a 4 -f 28G read
 
 .\vna_cw.py -f 29G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 29G read
+.\438a.py -a 4 -f 29G read
 
 .\vna_cw.py -f 30G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 30G read
+.\438a.py -a 4 -f 30G read
 
 .\vna_cw.py -f 31G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 31G read
+.\438a.py -a 4 -f 31G read
 
 .\vna_cw.py -f 32G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 32G read
+.\438a.py -a 4 -f 32G read
 
 .\vna_cw.py -f 33G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 33G read
+.\438a.py -a 4 -f 33G read
 
 .\vna_cw.py -f 34G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 34G read
+.\438a.py -a 4 -f 34G read
 
 .\vna_cw.py -f 35G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 35G read
+.\438a.py -a 4 -f 35G read
 
 .\vna_cw.py -f 36G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 36G read
+.\438a.py -a 4 -f 36G read
 
 .\vna_cw.py -f 37G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 37G read
+.\438a.py -a 4 -f 37G read
 
 .\vna_cw.py -f 38G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 38G read
+.\438a.py -a 4 -f 38G read
 
 .\vna_cw.py -f 39G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 39G read
+.\438a.py -a 4 -f 39G read
 
 .\vna_cw.py -f 39.5G -d -20.0 -p 2
 timeout 1 >nul
-.\m6960.py -f 39.5G read
+.\438a.py -a 4 -f 39.5G read
 
